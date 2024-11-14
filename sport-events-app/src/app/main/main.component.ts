3670cdaa295d9468dfc1826cef8faa617c372afd
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-14 17:43:28 +0000

    main component imports all-events-page

--- sport-events-app/src/app/main/main.component.ts
+++ sport-events-app/src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomePageComponent],
+  imports: [HomePageComponent, AllEventsPageComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
