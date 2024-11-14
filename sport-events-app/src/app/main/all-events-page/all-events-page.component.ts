d15046b224ece730f4242f4b4bc828b929d8b87b
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-14 17:39:14 +0000

    all-events-page compoment created - also html and css added

---
+++ sport-events-app/src/app/main/all-events-page/all-events-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-events-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-events-page.component.html',
+  styleUrl: './all-events-page.component.css'
+})
+export class AllEventsPageComponent {
+
+}
