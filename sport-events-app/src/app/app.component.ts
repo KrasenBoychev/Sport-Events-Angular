76f3dea1223ec549651e00ebdc8a42e201c44ce5
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-16 19:38:36 +0000

    app component - routes and imports updated

--- sport-events-app/src/app/app.component.ts
+++ sport-events-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent, AllEventsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- sport-events-app/src/app/app.routes.ts
+++ sport-events-app/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
+import { EventDetailsPageComponent } from './event-details-page/event-details-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomePageComponent },
+    { path: 'all-events', component: EventDetailsPageComponent },
+
+    // {
+    //     path: '',
+    //     children: [
+    //       { path: '', component: MainComponent },
+    //       {
+    //         // path: 'details/:id',
+    //         // component: UserDetailsComponent,
+    //         // canActivate: [AuthGuard],
+    //         // resolve: { user: UserDetailsResolver },
+    //       },
+    //     ],
+    //   },
+
+    {
+        path: '**',
+        component: ErrorPageComponent,
+    },
+];
