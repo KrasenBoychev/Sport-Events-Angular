0b4856ffa2182cb614fbe4fa4e38fba1dd7c5ffa
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-16 20:33:14 +0000

    all-events-page component created and html added

--- sport-events-app/src/app/app.component.ts
+++ sport-events-app/src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomePageComponent, AllEventsPageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
