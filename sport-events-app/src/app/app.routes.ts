93a494bb5143ffc36d23431e926ce18626d200ec
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-23 11:17:09 +0000

    login and register routes added

--- sport-events-app/src/app/app.routes.ts
+++ sport-events-app/src/app/app.routes.ts
@@ -3,27 +3,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
 import { EventDetailsPageComponent } from './event-details-page/event-details-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'all-events', component: EventDetailsPageComponent },
-
-    // {
-    //     path: '',
-    //     children: [
-    //       { path: '', component: MainComponent },
-    //       {
-    //         // path: 'details/:id',
-    //         // component: UserDetailsComponent,
-    //         // canActivate: [AuthGuard],
-    //         // resolve: { user: UserDetailsResolver },
-    //       },
-    //     ],
-    //   },
-
-    {
-        path: '**',
-        component: ErrorPageComponent,
-    },
+    { path: 'login', component: LoginPageComponent},
+    { path: 'register', component: RegisterPageComponent },
+    { path: '404', component: ErrorPageComponent },
+    { path: '**', redirectTo: '/404' },
 ];
