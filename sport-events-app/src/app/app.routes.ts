1ebeeff30bc83decf88b083aff1ec3245fa2cb79
Krasen Boychev <krasenboychev11@gmail.com>
2024-11-24 10:51:02 +0000

    imports updated

--- sport-events-app/src/app/app.routes.ts
+++ sport-events-app/src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AllEventsPageComponent } from './all-events-page/all-events-page.component';
 import { EventDetailsPageComponent } from './event-details-page/event-details-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './user/login-page/login-page.component';
+import { RegisterPageComponent } from './user/register-page/register-page.component';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
