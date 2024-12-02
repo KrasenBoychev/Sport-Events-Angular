47bb5ca2c00cac712c9adb57aca2c6a99c66216d
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-02 19:38:53 +0000

    userModel doesn't have tel, themes, posts properties

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,9 +5,6 @@ const saltRounds = Number(process.env.SALTROUNDS) || 5;
 const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new mongoose.Schema({
-    tel: {
-        type: String,
-    },
     email: {
         type: String,
         required: true,
@@ -35,15 +32,15 @@ const userSchema = new mongoose.Schema({
             },
             message: props => `${props.value} must contains only latin letters and digits!`
         },
-    },
-    themes: [{
-        type: ObjectId,
-        ref: "Theme"
-    }],
-    posts: [{
-        type: ObjectId,
-        ref: "Post"
-    }]
+    }
+    // themes: [{
+    //     type: ObjectId,
+    //     ref: "Theme"
+    // }],
+    // posts: [{
+    //     type: ObjectId,
+    //     ref: "Post"
+    // }]
 }, { timestamps: { createdAt: 'created_at' } });
 
 userSchema.methods = {
