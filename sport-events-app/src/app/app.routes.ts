4b1ce83a495af7478c08a9b366474d00c1288ae1
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-02 19:48:40 +0000

    routes updated; correct components imported

--- sport-events-app/src/app/app.routes.ts
+++ sport-events-app/src/app/app.routes.ts
@@ -5,41 +5,47 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { AddThemeComponent } from './theme/add-theme/add-theme.component';
-import { MainComponent } from './main/main.component';
 import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ErrorMsgComponent } from './core/error-msg/error-msg.component';
+import { NonAuthGuard } from './guards/nonAuth.guard';
+import { ThemesListComponent } from './theme/themes-list/themes-list.component';
+import { AllEventsComponent } from './event/all-events/all-events.component';
+import { AddEventComponent } from './event/add-event/add-event.component';
+import { SingleEventComponent } from './event/single-event/single-event.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
 
   //   Start - User routing
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate:[NonAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate:[NonAuthGuard] },
   { path: 'profile', component: ProfileComponent },
+  // { path: 'all-events', component: AllEventsComponent},
+  { path: 'add-event', component: AddEventComponent},
+
   //   End - User routing
 
   // Start - Theme routing
   {
-    path: 'themes',
+    path: 'all-events',
     children: [
-      { path: '', component: MainComponent },
+      { path: '', component: AllEventsComponent },
       {
-        path: ':themeId',
-        component: CurrentThemeComponent,
+        path: ':eventId',
+        component: SingleEventComponent,
         canActivate: [AuthGuard],
       },
     ],
   },
-  {
-    path: 'add-theme',
-    loadComponent: () =>
-      import('./theme/add-theme/add-theme.component').then(
-        (c) => c.AddThemeComponent
-      ),
-    canActivate: [AuthGuard],
-  },
+  //   path: 'add-theme',
+  //   loadComponent: () =>
+  //     import('./theme/add-theme/add-theme.component').then(
+  //       (c) => c.AddThemeComponent
+  //     ),
+  //   // canActivate: [AuthGuard],
+  // },
   // End - Theme routing
 
   { path: 'error', component: ErrorMsgComponent },
