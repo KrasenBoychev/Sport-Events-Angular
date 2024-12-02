a69ef9f8aacaa16f0e66845a569c345ffdd33ac2
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-02 19:47:20 +0000

    all paths for the server requests are correct; Event type imported

--- sport-events-app/src/app/api.service.ts
+++ sport-events-app/src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './types/post';
-import { Theme } from './types/theme';
+import { Event } from './types/event';
 
 @Injectable({
   providedIn: 'root',
@@ -18,33 +18,41 @@ export class ApiService {
     return this.http.get<Post[]>(url);
   }
 
-  getThemes() {
-    return this.http.get<Theme[]>(`/api/themes`);
+  // getThemes() {
+  //   return this.http.get<Theme[]>(`/api/themes`);
+  // }
+
+  getEvents() {
+    return this.http.get<Event[]>('/api/events');
   }
 
-  getSingleTheme(id: string) {
-    return this.http.get<Theme>(`/api/themes/${id}`);
+  // getSingleTheme(id: string) {
+  //   return this.http.get<Theme>(`/api/themes/${id}`);
+  // }
+
+  getSingleEvent(id: string) {
+    return this.http.get<Event>(`/api/events/${id}`);
   }
 
-  createTheme(themeName: string, postText: string) {
-    const payload = { themeName, postText };
-    return this.http.post<Theme>(`/api/themes`, payload);
+  createEvent(name: string, date: Date, time: string, place: string) {
+    const payload = { name, date, time, place };
+    return this.http.post<Event>(`/api/events`, payload);
   }
 
   // CRUD operations
   // update -> http.put
-  updateTheme(themeId: string, themeName: string, postText: string) {
-    const payload = { themeName, postText };
-    return this.http.put<Theme>(`/api/themes/${themeId}`, payload);
-  }
+  // updateTheme(themeId: string, themeName: string, postText: string) {
+  //   const payload = { themeName, postText };
+  //   return this.http.put<Theme>(`/api/themes/${themeId}`, payload);
+  // }
 
-  updatePost(themeId: string, postId: string) {
-    const payload = {};
-    return this.http.put<Theme>(
-      `/api/themes/${themeId}/posts/${postId}`,
-      payload
-    );
-  }
+  // updatePost(themeId: string, postId: string) {
+  //   const payload = {};
+  //   return this.http.put<Theme>(
+  //     `/api/themes/${themeId}/posts/${postId}`,
+  //     payload
+  //   );
+  // }
 
   // delete -> http.delete theme ID
   deletePost(themeId: string, postId: string) {
