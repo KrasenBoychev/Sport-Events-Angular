0402f2e910a5e132f81e0c135ebf7fa07665d14a
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-03 20:03:03 +0000

    added logic to add-event component

--- sport-events-app/src/app/event/add-event/add-event.component.ts
+++ sport-events-app/src/app/event/add-event/add-event.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-event',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-event.component.html',
   styleUrl: './add-event.component.css'
 })
 export class AddEventComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
 
+  addEvent(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { name, date, time, place, description } = form.value;
+
+    this.apiService.createEvent(name, date, time, place, description).subscribe(() => {
+      this.router.navigate(['/all-events']);
+    });
+  }
 }
