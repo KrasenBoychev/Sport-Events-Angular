8222d9b2935b21e9b8948c51478a9394c8bad276
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-06 08:48:40 +0000

    single-event component updated due to the usaged of the event-resolver; DatePipe imported

--- sport-events-app/src/app/event/single-event/single-event.component.ts
+++ sport-events-app/src/app/event/single-event/single-event.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { UserService } from '../../user/user.service';
 import { Event } from '../../types/event';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-single-event',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './single-event.component.html',
   styleUrl: './single-event.component.css'
 })
@@ -16,7 +17,6 @@ export class SingleEventComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private apiService: ApiService,
     private userService: UserService
   ) {}
 
@@ -24,15 +24,11 @@ export class SingleEventComponent {
     return this.userService.isLogged;
   }
 
-  get username(): string {
-    return this.userService.user?.username || '';
+  get userId(): string {        
+    return this.userService.user?._id || '';
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['eventId'];
-
-    this.apiService.getSingleEvent(id).subscribe((event: Event) => {
-      this.event = event;
-    });
+    this.event = this.route.snapshot.data['event'];
   }
 }
