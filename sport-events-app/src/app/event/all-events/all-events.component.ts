8f9d7d69ed24a0d881ea0357f35d4bf66a5f1ba5
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-06 08:48:08 +0000

    all-events component updated due to the usaged of the event-resolver

--- sport-events-app/src/app/event/all-events/all-events.component.ts
+++ sport-events-app/src/app/event/all-events/all-events.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LoaderComponent } from '../../shared/loader/loader.component';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { SlicePipe } from '../../shared/pipes/slice.pipe'; // If not in use - DELETE
 import { DatePipe } from '@angular/common';  // If not in use - DELETE
 import { ApiService } from '../../api.service';
@@ -17,12 +17,11 @@ export class AllEventsComponent {
   events: Event[] = [];
   isLoading = true;
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.apiService.getEvents().subscribe((events) => {
-      this.events = events;
-      this.isLoading = false;
-    });
+    this.events = this.route.snapshot.data['events'];
+    this.isLoading = false;
   }
 }
