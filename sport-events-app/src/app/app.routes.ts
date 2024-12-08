776214564dbeee58e32e887a0aec5ffd9922c050
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-08 10:13:32 +0000

    not needed imports and lines of code removed

--- sport-events-app/src/app/app.routes.ts
+++ sport-events-app/src/app/app.routes.ts
@@ -4,12 +4,9 @@ import { PageNotFoundComponent } from './error/error.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProfileComponent } from './user/profile/profile.component';
-import { AddThemeComponent } from './theme/add-theme/add-theme.component';
-import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ErrorMsgComponent } from './core/error-msg/error-msg.component';
 import { NonAuthGuard } from './guards/nonAuth.guard';
-import { ThemesListComponent } from './theme/themes-list/themes-list.component';
 import { AllEventsComponent } from './event/all-events/all-events.component';
 import { AddEventComponent } from './event/add-event/add-event.component';
 import { SingleEventComponent } from './event/single-event/single-event.component';
@@ -18,17 +15,9 @@ import { allEventsResolver, singleEventResolver } from './guards/event-resolvers
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-
-  //   Start - User routing
-  { path: 'login', component: LoginComponent, canActivate:[NonAuthGuard] },
-  { path: 'register', component: RegisterComponent, canActivate:[NonAuthGuard] },
-  { path: 'profile', component: ProfileComponent },
-  // { path: 'all-events', component: AllEventsComponent},
-  { path: 'add-event', component: AddEventComponent },
-
-  //   End - User routing
-
-  // Start - Theme routing
+  { path: 'login', component: LoginComponent, },
+  { path: 'register', component: RegisterComponent, },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   {
     path: 'all-events',
     children: [
@@ -41,15 +30,7 @@ export const routes: Routes = [
       },
     ],
   },
-  //   path: 'add-theme',
-  //   loadComponent: () =>
-  //     import('./theme/add-theme/add-theme.component').then(
-  //       (c) => c.AddThemeComponent
-  //     ),
-  //   // canActivate: [AuthGuard],
-  // },
-  // End - Theme routing
-
+  { path: 'add-event', component: AddEventComponent, canActivate: [AuthGuard] },
   { path: 'error', component: ErrorMsgComponent },
   { path: '404', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/404' },
