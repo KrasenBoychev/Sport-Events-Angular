6a9d3970513f9c72f15b58c8fd256b77988ee433
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-11 20:46:34 +0000

    app component holds the state of the ErrorMsgService

--- sport-events-app/src/app/app.component.ts
+++ sport-events-app/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
-
+import { ErrorMsgComponent } from "./core/error-msg/error-msg.component";
+import { ErrorMsgService } from './core/error-msg/error-msg.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,8 +13,15 @@ import { AuthenticateComponent } from './authenticate/authenticate.component';
     HeaderComponent,
     FooterComponent,
     AuthenticateComponent,
-  ],
+    ErrorMsgComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {};
+export class AppComponent {
+  error: string | null = null
+  
+  constructor(private errorMsgService: ErrorMsgService) {
+    this.errorMsgService.apiError$.subscribe(error => this.error = error);
+  }
+};
