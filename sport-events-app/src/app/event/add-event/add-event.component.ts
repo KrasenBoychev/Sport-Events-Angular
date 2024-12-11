386679481ff7463b15e0908111d28a32392b2d89
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-11 21:34:34 +0000

    getter Today added and const description added

--- sport-events-app/src/app/event/add-event/add-event.component.ts
+++ sport-events-app/src/app/event/add-event/add-event.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Router } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
-import { minDate } from '../../utils/setMinDate';
+import moment from 'moment';
 @Component({
   selector: 'app-add-event',
   standalone: true,
@@ -11,18 +11,20 @@ import { minDate } from '../../utils/setMinDate';
   styleUrl: './add-event.component.css'
 })
 export class AddEventComponent {
-  minDate = minDate();
+  get today() {
+    return moment().add(1, 'day').format('YYYY-MM-DD');
+  }
 
-  constructor(private apiService: ApiService, private router: Router) {}
+  constructor(private apiService: ApiService, private router: Router) { }
 
   addEvent(form: NgForm) {
     if (form.invalid) {
       return;
     }
 
-    const { name, date, time, place, textareaDescription } = form.value;
+    const { name, date, time, place, description } = form.value;
 
-    this.apiService.createEvent(name, date, time, place, textareaDescription).subscribe(() => {
+    this.apiService.createEvent(name, date, time, place, description).subscribe(() => {
       this.router.navigate(['/all-events']);
     });
   }
