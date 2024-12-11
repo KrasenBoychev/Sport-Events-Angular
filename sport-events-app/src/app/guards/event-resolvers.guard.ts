ed387aa78e462d1a2d8eb883502caa5bfad1b6b7
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-11 21:47:48 +0000

    userEventsResolver added

--- sport-events-app/src/app/guards/event-resolvers.guard.ts
+++ sport-events-app/src/app/guards/event-resolvers.guard.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Event } from '../types/event';
+import { UserService } from '../user/user.service';
 
 export const allEventsResolver: ResolveFn<Event[]> = (route) => {
     const apiService = inject(ApiService);
     return apiService.getEvents();
+};
+
+export const userEventsResolver: ResolveFn<Event[]> = (route) => {
+    const apiService = inject(ApiService);
+    const userService = inject(UserService);
+    let userId;
+    userService.user$.subscribe(user => userId = user?._id);
+    return apiService.getUserEvents(userId);
 };
 
 export const singleEventResolver: ResolveFn<Event> = (route) => {
