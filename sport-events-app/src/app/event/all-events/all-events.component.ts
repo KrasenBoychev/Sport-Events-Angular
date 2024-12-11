380f29f01ae0390b12d54b08555c6411296f8a9e
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-11 21:39:55 +0000

    not needed imports deleted

--- sport-events-app/src/app/event/all-events/all-events.component.ts
+++ sport-events-app/src/app/event/all-events/all-events.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { LoaderComponent } from '../../shared/loader/loader.component';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { SlicePipe } from '../../shared/pipes/slice.pipe'; // If not in use - DELETE
-import { DatePipe } from '@angular/common';  // If not in use - DELETE
-import { ApiService } from '../../api.service';
 import { Event } from '../../types/event';
 
 @Component({
   selector: 'app-all-events',
   standalone: true,
-  imports: [LoaderComponent, RouterLink, SlicePipe, DatePipe],
+  imports: [LoaderComponent, RouterLink],
   templateUrl: './all-events.component.html',
   styleUrl: './all-events.component.css'
 })
