6c52c597c63c54d3cb26eafd1ee3cf4a52f7e550
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-11 21:43:42 +0000

    single-event -> logic updated

--- sport-events-app/src/app/event/single-event/single-event.component.ts
+++ sport-events-app/src/app/event/single-event/single-event.component.ts
@@ -1,30 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { UserService } from '../../user/user.service';
 import { Event } from '../../types/event';
 import { DatePipe } from '@angular/common';
+import { DataService } from '../event.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-single-event',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './single-event.component.html',
   styleUrl: './single-event.component.css'
 })
-export class SingleEventComponent implements OnInit {
+export class SingleEventComponent implements OnInit, OnDestroy {
   event = {} as Event;
   hasJoined: boolean | null = null;
+  eventPeopleJoined: number | null = null;
+  isDeleteBtnClicked: boolean = false;
 
-  get userId(): string {        
+  get userId(): string {
     return this.userService.user?._id || '';
   }
 
+  message: Event | null = null;
+  subscription: Subscription | null = null;
+
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private userService: UserService,
+    private data: DataService,
     private apiService: ApiService,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.event = this.route.snapshot.data['event'];
@@ -34,18 +43,46 @@ export class SingleEventComponent implements OnInit {
     } else {
       this.hasJoined = false;
     }
+
+    this.eventPeopleJoined = this.event.usersJoined.length;
+
+    this.subscription = this.data.currentMessage.subscribe(message => this.message = message);
+  }
+
+  ngOnDestroy() {
+    this.subscription!.unsubscribe();
   }
 
   onJoinEvent() {
     this.apiService.addUserToEvent(this.userId, this.event._id).subscribe(() => {
+      this.eventPeopleJoined! += 1;
       this.hasJoined = true;
     });
   }
 
   onCancelEvent() {
     this.apiService.removeUserFromEvent(this.userId, this.event._id).subscribe(() => {
+      this.eventPeopleJoined! -= 1;
       this.hasJoined = false;
     });
   }
-  
+
+  onEditEvent() {
+    this.data.changeMessage(this.event);
+    this.router.navigate([`/edit-event/${this.event._id}`]);
+  }
+
+  onDeleteEvent() {
+    this.apiService.deleteEvent(this.event._id).subscribe(() => {
+      this.router.navigate([`/all-events`]);
+    });
+  }
+
+  onDeletePopUp() {
+    this.isDeleteBtnClicked = true;
+  }
+
+  onDeleteCancelPopUp() {
+    this.isDeleteBtnClicked = false;
+  }
 }
