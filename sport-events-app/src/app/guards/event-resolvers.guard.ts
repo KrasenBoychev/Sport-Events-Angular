9f61988ebff8ad77f79babeb6a2e50515d16be4d
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-22 09:58:46 +0000

    editEventResolver created

--- sport-events-app/src/app/guards/event-resolvers.guard.ts
+++ sport-events-app/src/app/guards/event-resolvers.guard.ts
@@ -1,8 +1,10 @@
-import { ResolveFn } from '@angular/router';
+import { ResolveFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Event } from '../types/event';
 import { UserService } from '../user/user.service';
+import { EventService } from '../event/event.service';
+import { Subscription } from 'rxjs';
 
 export const allEventsResolver: ResolveFn<Event[]> = (route) => {
     const apiService = inject(ApiService);
@@ -18,7 +20,28 @@ export const userEventsResolver: ResolveFn<Event[]> = (route) => {
 };
 
 export const singleEventResolver: ResolveFn<Event> = (route) => {
-const apiService = inject(ApiService);
-const id = route.paramMap.get('eventId')!;
-return apiService.getSingleEvent(id);
+    const apiService = inject(ApiService);
+    const id = route.paramMap.get('eventId')!;
+    return apiService.getSingleEvent(id);
+};
+
+export const editEventResolver: ResolveFn<{ currEvent: Event, subscription: Subscription}> = (route) => {
+    const eventService = inject(EventService);
+    const router = inject(Router);
+    const id = route.paramMap.get('eventId')!;
+
+    let subscription = null;
+    let currEvent = null;
+
+    subscription = eventService.currentEvent.subscribe(event => currEvent = event);
+
+    if (!currEvent) {
+        currEvent = JSON.parse(localStorage.getItem('eventDetails')!);
+    }
+
+    if (!currEvent) {
+        router.navigate([`/all-events/${id}`]);
+    }
+
+    return { currEvent, subscription };
 };
