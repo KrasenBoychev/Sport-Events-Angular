e6871243f8bb019ca7cfce0b35a44c87528a3794
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-23 10:48:33 +0000

    using Loader Component when a link from the navigation clicked and data fetches

--- sport-events-app/src/app/app.component.ts
+++ sport-events-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  RouterOutlet,
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { ErrorMsgComponent } from "./core/error-msg/error-msg.component";
 import { ErrorMsgService } from './core/error-msg/error-msg.service';
+import { LoaderComponent } from "./shared/loader/loader.component";
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,15 +22,36 @@ import { ErrorMsgService } from './core/error-msg/error-msg.service';
     HeaderComponent,
     FooterComponent,
     AuthenticateComponent,
-    ErrorMsgComponent
-],
+    ErrorMsgComponent,
+    LoaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  error: string | null = null
-  
-  constructor(private errorMsgService: ErrorMsgService) {
+  error: string | null = null;
+  loading = false;
+
+  constructor(private errorMsgService: ErrorMsgService, private router: Router) {
     this.errorMsgService.apiError$.subscribe(error => this.error = error);
+
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
   }
 };
